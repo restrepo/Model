5bb190b402a335b6a97ffdcb0f708df1be5f7c18
restrepo <restrepo@udea.edu.co>
2023-08-24 18:18:04 -0500

    run script

---
+++ xenonphobic.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#bash runmicromegas.sh && echo SPheno && read && bash runSpheno.sh
+if [ ! "$1" ];then
+    echo "Usage: $0 N[\\\\/M]"
+    exit
+fi
+d=$1
+if [ "$(echo $d | grep '/')" ]; then
+    if [ ! "$(echo $d | grep '\\/')" ]; then
+	echo "ERROR! use \\\\/ for /"
+	exit
+    fi
+fi
+sed -i 's/^\s*dRInput\s*\=\s*.*;/dRInput = '$d';/' SARAH/Models/Zprime/config.m
+echo "Compiling the model for dRInput=$d ..."
+bash diffkkk.sh
+grep -B1 -A1 "{Xq,Xl" kkk
+echo micromegas
+read
+bash runmicromegas.sh && echo SPheno && read && bash runSpheno.sh
+#TODO: Rescale gp to get the correct relic density
+mkdir -p ZprimeOut
+df=.$(echo $d | sed 's/\\\//_/')
+./SPheno/bin/SPhenoZprime LesHouches.in.Zprime
+./micromegas/Zprime/CalcOmega_with_DDetection_MOv5 SPheno.spc.Zprime > ZprimeOut/micromegas$df.out
+mv SPheno.spc.Zprime ZprimeOut/SPheno.spc.Zprime$df
