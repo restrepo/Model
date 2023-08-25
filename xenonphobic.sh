ae26cc4f8687ea80a2f813aae425aa21835ca037
restrepo <restrepo@udea.edu.co>
2023-08-24 19:48:54 -0500

    lc

--- xenonphobic.sh
+++ xenonphobic.sh
@@ -16,11 +16,15 @@ echo "Compiling the model for dRInput=$d ..."
 bash diffkkk.sh
 grep -B1 -A1 "{Xq,Xl" kkk
 echo micromegas
-read
-bash runmicromegas.sh && echo SPheno && read && bash runSpheno.sh
+#read
+bash runmicromegas.sh 
+echo SPheno 
+#read
+bash runSpheno.sh
 #TODO: Rescale gp to get the correct relic density
 mkdir -p ZprimeOut
 df=.$(echo $d | sed 's/\\\//_/')
 ./SPheno/bin/SPhenoZprime LesHouches.in.Zprime
 ./micromegas/Zprime/CalcOmega_with_DDetection_MOv5 SPheno.spc.Zprime > ZprimeOut/micromegas$df.out
 mv SPheno.spc.Zprime ZprimeOut/SPheno.spc.Zprime$df
+#cat micromegas.54_43.out | grep -A1000  ^Xf |  grep -E "^\s*\w" | grep -Ev '^\s*[0-9]' | grep -B1000 -A2 "cross sections"
